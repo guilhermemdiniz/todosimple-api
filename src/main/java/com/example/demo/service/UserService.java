db44dee5c071a1b0c9ccae43ba1a5e4f2d6efd68
Guilherme <guidiniz2019.18@gmail.com>
2026-02-22 11:42:58 -0300

    Create UserController

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,13 +1,14 @@
 package com.example.demo.service;
 
 import com.example.demo.models.User;
-import com.example.demo.repositories.TaskRepository;
 import com.example.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
